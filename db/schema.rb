f7c245d2b6b7329e2c4857d2d38773ee9382c5e6
“y-sawara” <fb.yosuke.13@gmail.com>
2020-01-05 12:08:46 +0900

    type your commit message here

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_092412) do
+ActiveRecord::Schema.define(version: 2020_01_05_023452) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_12_29_092412) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "ideas"
 end
